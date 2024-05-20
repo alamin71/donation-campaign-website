2c07b1942ff860b00eb32a6bdef4ff7027afe2d8
AL-AMIN <alamin50cse.miu@gmail.com>
2024-05-21 01:40:31 +0600

    pie chart ,statistics data add

--- src/Components/Alldonation.jsx
+++ src/Components/Alldonation.jsx
@@ -1,11 +1,40 @@
-
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { getLocalstorage } from "../Utilities/localStorage";
+import Donated from "./Donated";
 
 const Alldonation = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [donated, setDonated] = useState([]);
+  const [showall, setshowall] = useState(4);
+  const datas = useLoaderData();
+  useEffect(() => {
+    const storedData = getLocalstorage();
+    if (datas.length > 0) {
+      const donated = datas.filter((data) => storedData.includes(data.id));
+      setDonated(donated);
+    }
+  }, [datas]);
+  return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 mx-16 my-10 gap-5">
+        {donated.slice(0, showall).map((data, idx) => (
+          <Donated key={idx} data={data}></Donated>
+        ))}
+      </div>
+      <div
+        className={
+          (donated.length <= showall && "hidden") || "flex justify-center"
+        }
+      >
+        <button
+          onClick={() => setshowall(donated.length)}
+          className="btn btn-primary bg-[#009444] text-sada"
+        >
+          Show all
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Alldonation;
--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,33 @@ import {
 } from "react-router-dom";
 import Root from './Components/Root';
 import Home from './Components/Home';
+import ErrorPage from './Components/Error';
+import Alldonation from './Components/Alldonation';
+import Statistics from './Components/Statistics';
+import Details from './Components/Details';
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     children:[
       {
         path:'/',
         element: <Home></Home>,
+        loader: () => fetch("/data.json"),
+      },
+      {
+        path:'/all',
+        element:<Alldonation></Alldonation>,
+        loader: () => fetch("/data.json"),
+      },
+      {
+        path:'/stat',
+        element:<Statistics></Statistics>,
+      },
+      {
+        path: "/details/:id",
+        element: <Details></Details>,
         loader: () => fetch("/data.json"),
       }
     ]
